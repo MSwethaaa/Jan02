b310e0e3165b40b36f43d3b369e9823ff3f42e55
DELL <Swetha>
2024-01-04 15:22:09 +0530

    Add project files.

---
+++ Jan02/TestSealed.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jan02
+{
+  
+        sealed class TestSealed
+        {
+            private int num1 = 100;
+            public int Ans { get; set; }
+            public void GetData() { }
+        }
+        public class ClientSealed
+        {
+            static void Main(string[] args)
+            {
+                TestSealed ts = new TestSealed();
+                ts.GetData();
+
+            }
+        }
+    }
